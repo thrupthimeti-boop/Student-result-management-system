874516ee8af959442c163765f6dc17a2f8005c62
thrupthimeti-boop <thrupthimeti@gmail.com>
2026-07-01 15:23:27 +0530

    Add student feature

--- main.py
+++ main.py
@@ -11,11 +11,22 @@ while True:
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        print("Add Student feature coming next.")
+        student_id = input("Enter Student ID: ")
+        student_name = input("Enter Student Name: ")
+        marks = input("Enter Student Marks: ")
+
+        print("\nStudent Details")
+        print("ID:", student_id)
+        print("Name:", student_name)
+        print("Marks:", marks)
+        print("Student added successfully!")
+
     elif choice == "2":
         print("View Students feature coming next.")
+
     elif choice == "3":
         print("Thank you!")
         break
+
     else:
         print("Invalid choice. Try again.")
